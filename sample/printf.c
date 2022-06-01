041a3551eb495d3cd7715ad3ab61ddd9cb766102
Go Horikoshi <yryrgogo@gmail.com>
2022-06-01 22:09:59 +0000

    リンカについて勉強中

--- sample/printf.c
+++ sample/printf.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
+char c = 'a';
+int i0;
+int i1 = 0;
+static int si0;
+static int si1 = 0;
+const int ci0;
+const int ci1 = 0;
+char *p0;
+char *p1 = "sample0";
+
 int foo()
 {
 	printf("OK\n");
