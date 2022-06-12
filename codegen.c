3f50b6a39954fb75831a6e60c42eb51c2a5007d6
Go Horikoshi <yryrgogo@gmail.com>
2022-06-12 06:50:05 +0000

    add: codegen にコメントを追加

--- codegen.c
+++ codegen.c
@@ -5,15 +5,11 @@ static int count(void) {
   return i++;
 }
 
-void gen_lval(Node *node) {
-  if(node->kind != ND_LVAR)
-    error_at(__FILE__, "代入の左辺値が変数ではありません。");
-
-  printf("  mov rax, rbp\n");
-  printf("  sub rax, %d\n", node->offset);
-  printf("  push rax\n");
-}
-
+/**
+ * @brief コードの top level にある Node を処理する
+ *
+ * @param node
+ */
 void gen(Node *node) {
   switch(node->kind) {
   case ND_FUNC: {
@@ -21,169 +17,11 @@ void gen(Node *node) {
     break;
   }
   default: {
-    gen_stmt(node);
+    error_at(NULL, "not implemented.");
   }
   }
 
   return;
-}
-
-void gen_stmt(Node *node) {
-  switch(node->kind) {
-  case ND_RETURN:
-    gen_expr(node->lhs);
-    printf("  pop rax\n");
-    printf("  mov rsp, rbp\n");
-    printf("  pop rbp\n");
-    printf("  ret\n");
-    return;
-  case ND_BLOCK: {
-    Node *cur = node->body;
-    while(cur) {
-      gen_stmt(cur);
-      cur = cur->next;
-    }
-    return;
-  }
-  case ND_FOR: {
-    int c = count();
-
-    gen_expr(node->init);
-    printf("  pop rax\n");
-    printf("  jmp .L.for.begin.%d\n", c);
-
-    printf(".L.for.begin.%d:\n", c);
-    gen_expr(node->cond);
-    printf("  pop rax\n");
-    printf("  cmp rax, 0\n");
-    printf("  je .L.for.end.%d\n", c);
-    gen_stmt(node->then);
-    gen_expr(node->inc);
-    printf("  pop rax\n");
-    printf("  jmp .L.for.begin.%d\n", c);
-
-    printf(".L.for.end.%d:\n", c);
-    return;
-  }
-  case ND_WHILE: {
-    int c = count();
-
-    printf("  jmp .L.while.begin.%d\n", c);
-
-    printf(".L.while.begin.%d:\n", c);
-    gen_expr(node->cond);
-    printf("  pop rax\n");
-    printf("  cmp rax, 0\n");
-    printf("  je .L.while.end.%d\n", c);
-    gen_stmt(node->then);
-    printf("  jmp .L.while.begin.%d\n", c);
-
-    printf(".L.while.end.%d:\n", c);
-    return;
-  }
-  case ND_IF: {
-    int c = count();
-    gen_expr(node->cond);
-    printf("  pop rax\n");
-    printf("  cmp rax, 0\n");
-    printf("  je .L.else.%d\n", c);
-
-    gen_stmt(node->then);
-
-    printf("  jmp .L.end.%d\n", c);
-    printf(".L.else.%d:\n", c);
-    if(node->els) {
-      gen_stmt(node->els);
-    }
-
-    printf(".L.end.%d:\n", c);
-    return;
-  }
-  case ND_NOP: {
-    printf("  nop\n");
-    return;
-  }
-  default:
-    gen_expr(node);
-    printf("  pop rax\n");
-  }
-
-  return;
-}
-
-void gen_calculator(Node *node) {
-  if(node->lhs && node->rhs) {
-    gen_expr(node->lhs);
-    gen_expr(node->rhs);
-    printf("  pop rdi\n");
-    printf("  pop rax\n");
-  }
-
-  switch(node->kind) {
-  case ND_ADD:
-    printf("  add rax, rdi\n");
-    break;
-  case ND_SUB:
-    printf("  sub rax, rdi\n");
-    break;
-  case ND_MUL:
-    printf("  imul rax, rdi\n");
-    break;
-  case ND_DIV:
-    printf("  cqo\n");
-    printf("  idiv rdi\n");
-    break;
-  case ND_EQ:
-    printf("  cmp rax, rdi\n");
-    printf("  sete al\n");
-    printf("  movzb rax, al\n");
-    break;
-  case ND_NE:
-    printf("  cmp rax, rdi\n");
-    printf("  setne al\n");
-    printf("  movzb rax, al\n");
-    break;
-  case ND_LT:
-    printf("  cmp rax, rdi\n");
-    printf("  setl al\n");
-    printf("  movzb rax, al\n");
-    break;
-  case ND_LE:
-    printf("  cmp rax, rdi\n");
-    printf("  setle al\n");
-    printf("  movzb rax, al\n");
-    break;
-  }
-}
-
-void gen_expr(Node *node) {
-  switch(node->kind) {
-  case ND_NUM:
-    printf("  push %d\n", node->val);
-    return;
-  case ND_LVAR:
-    gen_lval(node);
-    printf("  pop rax\n");
-    printf("  mov rax, [rax]\n");
-    printf("  push rax\n");
-    return;
-  case ND_ASSIGN:
-    gen_lval(node->lhs);
-    gen_expr(node->rhs);
-    printf("  pop rdi\n");
-    printf("  pop rax\n");
-    printf("  mov [rax], rdi\n");
-    printf("  push rax\n");
-    return;
-  case ND_FUNC_CALL:
-    gen_func_call(node);
-    printf("  push rax\n");
-    return;
-  default:
-    gen_calculator(node);
-    printf("  push rax\n");
-    return;
-  }
 }
 
 void gen_func(Node *node) {
@@ -243,6 +81,198 @@ void gen_func(Node *node) {
   printf("  ret\n");
 
   return;
+}
+
+/**
+ * @brief statement は expression が stack
+ * に残した値を使って処理を組み立てる。gen_expr() のあとは pop rax で式の結果を
+ * rax にセットし後続処理を行うことが多い
+ *
+ * @param node
+ */
+void gen_stmt(Node *node) {
+  switch(node->kind) {
+  case ND_RETURN:
+    gen_expr(node->lhs);
+    printf("  pop rax\n");
+    printf("  mov rsp, rbp\n");
+    printf("  pop rbp\n");
+    printf("  ret\n");
+    return;
+  case ND_BLOCK: {
+    Node *cur = node->body;
+    while(cur) {
+      gen_stmt(cur);
+      cur = cur->next;
+    }
+    return;
+  }
+  case ND_FOR: {
+    int c = count();
+
+    gen_expr(node->init);
+    printf("  pop rax\n");
+    printf("  jmp .L.for.begin.%d\n", c);
+
+    printf(".L.for.begin.%d:\n", c);
+    gen_expr(node->cond);
+    printf("  pop rax\n");
+    printf("  cmp rax, 0\n");
+    printf("  je .L.for.end.%d\n", c);
+
+    gen_stmt(node->then);
+
+    gen_expr(node->inc);
+    printf("  pop rax\n");
+    printf("  jmp .L.for.begin.%d\n", c);
+
+    printf(".L.for.end.%d:\n", c);
+    return;
+  }
+  case ND_WHILE: {
+    int c = count();
+
+    printf("  jmp .L.while.begin.%d\n", c);
+    printf(".L.while.begin.%d:\n", c);
+    gen_expr(node->cond);
+    printf("  pop rax\n");
+    printf("  cmp rax, 0\n");
+    printf("  je .L.while.end.%d\n", c);
+
+    gen_stmt(node->then);
+    printf("  jmp .L.while.begin.%d\n", c);
+
+    printf(".L.while.end.%d:\n", c);
+    return;
+  }
+  case ND_IF: {
+    int c = count();
+    gen_expr(node->cond);
+    printf("  pop rax\n");
+    printf("  cmp rax, 0\n");
+    printf("  je .L.else.%d\n", c);
+
+    gen_stmt(node->then);
+
+    printf("  jmp .L.end.%d\n", c);
+    printf(".L.else.%d:\n", c);
+    if(node->els) {
+      gen_stmt(node->els);
+    }
+
+    printf(".L.end.%d:\n", c);
+    return;
+  }
+  case ND_NOP: {
+    printf("  nop\n");
+    return;
+  }
+  default:
+    gen_expr(node);
+    printf("  pop rax\n");
+  }
+
+  return;
+}
+
+/**
+ * @brief expression は式のため、値を stack に1つ残す
+ *
+ * @param node
+ */
+void gen_expr(Node *node) {
+  switch(node->kind) {
+  case ND_NUM:
+    printf("  push %d\n", node->val);
+    return;
+  case ND_LVAR:
+    gen_lval(node);
+    printf("  pop rax\n");
+    printf("  mov rax, [rax]\n");
+    printf("  push rax\n");
+    return;
+  case ND_ASSIGN:
+    gen_lval(node->lhs);
+    gen_expr(node->rhs);
+    printf("  pop rdi\n");
+    printf("  pop rax\n");
+    printf("  mov [rax], rdi\n");
+    printf("  push rax\n");
+    return;
+  case ND_FUNC_CALL:
+    gen_func_call(node);
+    printf("  push rax\n");
+    return;
+  default:
+    gen_calculator(node);
+    printf("  push rax\n");
+    return;
+  }
+}
+
+/**
+ * @brief rax, rdi を使って演算を行う
+ *
+ * @param node
+ */
+void gen_calculator(Node *node) {
+  if(node->lhs && node->rhs) {
+    gen_expr(node->lhs);
+    gen_expr(node->rhs);
+    printf("  pop rdi\n");
+    printf("  pop rax\n");
+  }
+
+  switch(node->kind) {
+  case ND_ADD:
+    printf("  add rax, rdi\n");
+    break;
+  case ND_SUB:
+    printf("  sub rax, rdi\n");
+    break;
+  case ND_MUL:
+    printf("  imul rax, rdi\n");
+    break;
+  case ND_DIV:
+    printf("  cqo\n");
+    printf("  idiv rdi\n");
+    break;
+  case ND_EQ:
+    printf("  cmp rax, rdi\n");
+    printf("  sete al\n");
+    printf("  movzb rax, al\n");
+    break;
+  case ND_NE:
+    printf("  cmp rax, rdi\n");
+    printf("  setne al\n");
+    printf("  movzb rax, al\n");
+    break;
+  case ND_LT:
+    printf("  cmp rax, rdi\n");
+    printf("  setl al\n");
+    printf("  movzb rax, al\n");
+    break;
+  case ND_LE:
+    printf("  cmp rax, rdi\n");
+    printf("  setle al\n");
+    printf("  movzb rax, al\n");
+    break;
+  }
+}
+
+/**
+ * @brief ベースポインタからのオフセットでローカル変数のアドレスを算出し、stack
+ * push する
+ *
+ * @param node
+ */
+void gen_lval(Node *node) {
+  if(node->kind != ND_LVAR)
+    error_at(__FILE__, "代入の左辺値が変数ではありません。");
+
+  printf("  mov rax, rbp\n");
+  printf("  sub rax, %d\n", node->offset);
+  printf("  push rax\n");
 }
 
 void gen_func_call(Node *node) {
