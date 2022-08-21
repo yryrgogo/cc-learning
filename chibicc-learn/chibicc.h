3c0c866d112072371385991766454ec381f81320
Go Horikoshi <yryrgogo@gmail.com>
2022-08-21 06:49:10 +0000

    [chibicc] b4e82cf: Add keyword int and make variable definition mandatory

--- chibicc-learn/chibicc.h
+++ chibicc-learn/chibicc.h
@@ -37,6 +37,7 @@ void error_at(char *loc, char *fmt, ...);
 void error_tok(Token *tok, char *fmt, ...);
 bool equal(Token *tok, char *op);
 Token *skip(Token *tok, char *s);
+bool consume(Token **rest, Token *tok, char *str);
 Token *tokenize(char *p);
 
 //
@@ -48,6 +49,7 @@ typedef struct Obj Obj;
 struct Obj {
   Obj *next;
   char *name;
+  Type *ty;
   int offset;
 };
 
@@ -120,12 +122,18 @@ typedef enum {
 
 struct Type {
   TypeKind kind;
+
+  // Pointer
   Type *base;
+
+  // Declaration
+  Token *name;
 };
 
 extern Type *ty_int;
 
 bool is_integer(Type *ty);
+Type *pointer_to(Type *base);
 void add_type(Node *node);
 
 //
--- chibicc-learn/parse.c
+++ chibicc-learn/parse.c
@@ -4,6 +4,7 @@
 // list.
 Obj *locals;
 
+static Node *declaration(Token **rest, Token *tok);
 static Node *compound_stmt(Token **rest, Token *tok);
 static Node *stmt(Token **rest, Token *tok);
 static Node *expr_stmt(Token **rest, Token *tok);
@@ -56,12 +57,69 @@ static Node *new_var_node(Obj *var, Token *tok) {
   return node;
 }
 
-static Obj *new_lvar(char *name) {
+static Obj *new_lvar(char *name, Type *ty) {
   Obj *var = calloc(1, sizeof(Obj));
   var->name = name;
   var->next = locals;
+  var->ty = ty;
   locals = var;
   return var;
+}
+
+static char *get_ident(Token *tok) {
+  if(tok->kind != TK_IDENT)
+    error_tok(tok, "expected an identifier");
+  return strndup(tok->loc, tok->len);
+}
+
+// declspec = "int"
+static Type *declspec(Token **rest, Token *tok) {
+  *rest = skip(tok, "int");
+  return ty_int;
+}
+
+// declarator = "*"* ident
+static Type *declarator(Token **rest, Token *tok, Type *ty) {
+  while(consume(&tok, tok, "*"))
+    ty = pointer_to(ty);
+
+  if(tok->kind != TK_IDENT)
+    error_tok(tok, "expected a variable name");
+
+  ty->name = tok;
+  *rest = tok->next;
+  return ty;
+}
+
+// declaration = declspec (declarator ("=" expr)? ("," declarator ("="
+// expr)?)*)? ";"
+static Node *declaration(Token **rest, Token *tok) {
+  Type *basety = declspec(&tok, tok);
+
+  Node head = {};
+  Node *cur = &head;
+  int i = 0;
+
+  while(!equal(tok, ";")) {
+    if(i++ > 0)
+      tok = skip(tok, ",");
+
+    Type *ty = declarator(&tok, tok, basety);
+    Obj *var = new_lvar(get_ident(ty->name), ty);
+
+    if(!equal(tok, "="))
+      continue;
+
+    Node *lhs = new_var_node(var, ty->name);
+    Node *rhs = assign(&tok, tok->next);
+    Node *node = new_binary(ND_ASSIGN, lhs, rhs, tok);
+    cur = cur->next = new_unary(ND_EXPR_STMT, node, tok);
+  }
+
+  Node *node = new_node(ND_BLOCK, tok);
+  node->body = head.next;
+  *rest = tok->next;
+  return node;
 }
 
 // stmt = "return" expr ";"
@@ -123,13 +181,16 @@ static Node *stmt(Token **rest, Token *tok) {
   return expr_stmt(rest, tok);
 }
 
-// compound-stmt = stmt* "}"
+// compound-stmt = (declaration | stmt)* "}"
 static Node *compound_stmt(Token **rest, Token *tok) {
   Node head = {};
   Node *cur = &head;
 
   while(!equal(tok, "}")) {
-    cur = cur->next = stmt(&tok, tok);
+    if(equal(tok, "int"))
+      cur = cur->next = declaration(&tok, tok);
+    else
+      cur = cur->next = stmt(&tok, tok);
     add_type(cur);
   }
 
@@ -348,7 +409,8 @@ static Node *primary(Token **rest, Token *tok) {
   if(tok->kind == TK_IDENT) {
     Obj *var = find_var(tok);
     if(!var)
-      var = new_lvar(strndup(tok->loc, tok->len));
+      // NOTE: 変数の宣言・定義は declaration でまとめて行っているため、ここで変数が見つからないということはあり得ない実装になっている
+      error_tok(tok, "undefined variable");
     *rest = tok->next;
     return new_var_node(var, tok);
   }
